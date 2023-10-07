3be1d5d271ac935f1a658fa9479f17e00fdfe4ba
MichaelShawUK <MichaelShawUK@gmail.com>
2023-10-07 12:14:52 +0100

    Update title

--- src/projects/junior/newsletter-sign-up-with-success-message-main/src/App.tsx
+++ src/projects/junior/newsletter-sign-up-with-success-message-main/src/App.tsx
@@ -3,7 +3,7 @@ import "./styles/css/index.css";
 function App() {
   return (
     <article className="app">
-      <h4>Title</h4>
+      <h4>Stay Updated</h4>
       <p>Newsletter</p>
     </article>
   );
