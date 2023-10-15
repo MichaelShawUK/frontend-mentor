d5a8225346f9510e006975f4b5ee54437e65b606
MichaelShawUK <MichaelShawUK@gmail.com>
2023-10-15 21:34:31 +0100

    Clean up App return value

--- src/projects/junior/newsletter-sign-up-with-success-message-main/src/App.tsx
+++ src/projects/junior/newsletter-sign-up-with-success-message-main/src/App.tsx
@@ -1,6 +1,4 @@
 import "./styles/css/index.css";
-import illustrationMobile from "./assets/images/illustration-sign-up-mobile.svg";
-import illustrationDesktop from "./assets/images/illustration-sign-up-desktop.svg";
 import SignUp from "./components/SignUp";
 import Success from "./components/Success";
 
@@ -9,36 +7,7 @@ import { useState } from "react";
 function App() {
   const [isSubscribed, setIsSubscribed] = useState(false);
 
-  const content = isSubscribed ? <Success /> : <SignUp />;
-
-  return content;
-
-  // return (
-  //   <div className="container">
-  //     <article className="app">
-  //       <section className="sign-up">
-  //         <h1>Stay Updated!</h1>
-  //         <p>Join 60,000+ product managers receiving monthly updates on:</p>
-  //         <ul>
-  //           <li>Product discovery and building what matters</li>
-  //           <li>Measuring to ensure updates are a success</li>
-  //           <li>And much more!</li>
-  //         </ul>
-  //         <form >
-  //           <div className="field">
-  //             <label htmlFor="email">Email address</label>
-  //             <input type="text" id="email" placeholder="email@company.com"/>
-  //           </div>
-  //           <button>Subscribe to monthly newsletter</button>
-  //         </form>
-  //       </section>
-  //       <section className="illustration">
-  //         <img className="desktop" src={illustrationDesktop} />
-  //         <img className="mobile" src={illustrationMobile} />
-  //       </section>
-  //     </article>
-  //   </div>
-  // );
+  return isSubscribed ? <Success /> : <SignUp />;
 }
 
 export default App;
