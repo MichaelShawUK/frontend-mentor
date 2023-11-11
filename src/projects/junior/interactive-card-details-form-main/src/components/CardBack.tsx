ecfd136b9b849a30597bb9123c1d7da128666855
MichaelShawUK <MichaelShawUK@gmail.com>
2023-11-11 13:52:32 +0000

    Display CVC on back of card

--- src/projects/junior/interactive-card-details-form-main/src/components/CardBack.tsx
+++ src/projects/junior/interactive-card-details-form-main/src/components/CardBack.tsx
@@ -1,7 +1,14 @@
+import AppContext from "../state/context";
+
+import { useContext } from "react";
+
 function CardBack() {
+  const { state } = useContext(AppContext);
+  const cvc = state.enteredValues.cvc ? state.enteredValues.cvc : "123";
+
   return (
     <div className="card-back">
-      <p className="cvc-strip">000</p>
+      <p className="cvc-strip">{cvc}</p>
     </div>
   );
 }
