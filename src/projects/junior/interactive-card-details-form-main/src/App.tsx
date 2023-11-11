54ec64c9478749137d5c69eb8f53e3cbaf7b1316
MichaelShawUK <MichaelShawUK@gmail.com>
2023-11-11 12:11:41 +0000

    Position components for desktop view

--- src/projects/junior/interactive-card-details-form-main/src/App.tsx
+++ src/projects/junior/interactive-card-details-form-main/src/App.tsx
@@ -2,10 +2,12 @@ import "./styles/css/index.css";
 
 import InteractiveCards from "./components/InteractiveCards";
 import CardForm from "./components/CardForm";
+import desktopBg from "./assets/images/bg-main-desktop.png";
 
 function CardDetailsForm() {
   return (
     <div className="card-details-form">
+      <img src={desktopBg} className="desktop-bg" />
       <InteractiveCards />
       <CardForm />
     </div>
