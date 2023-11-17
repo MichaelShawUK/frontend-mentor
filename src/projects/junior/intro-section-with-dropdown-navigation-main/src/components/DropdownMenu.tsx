78b23ed2c94635e390deaadca5d1985150042340
MichaelShawUK <MichaelShawUK@gmail.com>
2023-11-17 13:02:26 +0000

    Toggle dropdown menu on mouse click

--- src/projects/junior/intro-section-with-dropdown-navigation-main/src/components/DropdownMenu.tsx
+++ src/projects/junior/intro-section-with-dropdown-navigation-main/src/components/DropdownMenu.tsx
@@ -3,10 +3,36 @@ import calendarIcon from "../assets/images/icon-calendar.svg";
 import reminderIcon from "../assets/images/icon-reminders.svg";
 import planningIcon from "../assets/images/icon-planning.svg";
 
+import { useState, useEffect, useRef, useCallback } from "react";
+
 function DropdownMenu() {
+  const [showMenu, setShowMenu] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  function clickHandler(event: UIEvent) {
+    if (showMenu) {
+      setShowMenu(false);
+    }
+    if (event.target === buttonRef.current && !showMenu) {
+      setShowMenu(true);
+    }
+  }
+  const cacheClickHandler = useCallback(clickHandler, [showMenu]);
+
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("click", cacheClickHandler);
+
+    return () => {
+      window.removeEventListener("click", cacheClickHandler);
+    };
+  }, [showMenu, cacheClickHandler]);
+
+  const className = showMenu ? "show dropdown" : "dropdown";
+
   return (
-    <div className="dropdown">
-      <button>Features</button>
+    <div className={className}>
+      <button ref={buttonRef}>Features</button>
       <div className="links">
         <ul>
           <li>
