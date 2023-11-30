7f6b0bbbb0bbea26c749b1987fa8d477636b7220
MichaelShawUK <MichaelShawUK@gmail.com>
2023-11-30 14:50:24 +0000

    Fade in advice on page load

--- src/projects/junior/advice-generator-app-main/src/App.tsx
+++ src/projects/junior/advice-generator-app-main/src/App.tsx
@@ -3,19 +3,43 @@ import dice from "../src/assets/images/icon-dice.svg";
 import mobileDivider from "../src/assets/images/pattern-divider-mobile.svg";
 import desktopDivider from "../src/assets/images/pattern-divider-desktop.svg";
 import useViewMode from "./hooks/useViewMode";
+import { useEffect, useState } from "react";
+
+interface IAdvice {
+  id: number;
+  advice: string;
+}
 
 function AdviceGenerator() {
   const viewMode = useViewMode();
   const divider = viewMode === "MOBILE" ? mobileDivider : desktopDivider;
 
+  const [response, setResponse] = useState<IAdvice>({
+    id: 0,
+    advice: "",
+  });
+
+  async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setResponse(data.slip);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div className="flex-wrapper">
       <div className="advice-generator">
-        <p>ADVIDE GENERATOR</p>
-        <button className="dice-btn">
+        <div className="advice">
+          <h6>ADVICE #{response.id}</h6>
+          <p>"{response.advice}"</p>
+          <img src={divider} className="divider" />
+        </div>
+        <button className="dice-btn" onClick={() => getAdvice()}>
           <img src={dice} />
         </button>
-        <img src={divider} />
       </div>
     </div>
   );
