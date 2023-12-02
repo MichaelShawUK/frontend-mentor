2ddee9884c42f3ec7f9837fc928b5eefb838357b
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-02 13:15:52 +0000

    Layout profile card

--- src/projects/junior/time-tracking-dashboard-main/src/App.tsx
+++ src/projects/junior/time-tracking-dashboard-main/src/App.tsx
@@ -1,9 +1,12 @@
 import "../src/styles/css/index.css";
+import ProfileCard from "./components/ProfileCard";
 import TrackingCard from "./components/TrackingCard";
 
 function TimeTrackingDashboard() {
   return (
     <div className="time-tracking-dashboard">
+      <ProfileCard />
+      <TrackingCard />
       <TrackingCard />
       {/* <TrackingCard /> */}
     </div>
--- src/projects/junior/time-tracking-dashboard-main/src/components/ProfileCard.tsx
+++ src/projects/junior/time-tracking-dashboard-main/src/components/ProfileCard.tsx
@@ -1,5 +1,21 @@
+import avatar from "../assets/images/image-jeremy.png";
+
 function ProfileCard() {
-  return <div className="profile card"></div>;
+  return (
+    <div className="profile">
+      <div className="avatar">
+        <img src={avatar} />
+        <p>
+          Report for <span className="report-name">Jeremey Robson</span>
+        </p>
+      </div>
+      <ul>
+        <li>Daily</li>
+        <li>Weekly</li>
+        <li>Monthly</li>
+      </ul>
+    </div>
+  );
 }
 
 export default ProfileCard;
