03714b64f55bd37e8c40b103a2b5adecf6c40875
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-08 17:26:37 +0000

    Display custom tip input on custom label click

--- src/projects/junior/tip-calculator-app-main/src/App.tsx
+++ src/projects/junior/tip-calculator-app-main/src/App.tsx
@@ -3,7 +3,62 @@ import logo from "./assets/images/logo.svg";
 import dollarIcon from "../src/assets/images/icon-dollar.svg";
 import personIcon from "../src/assets/images/icon-person.svg";
 
+import { useState } from "react";
+
 function TipCalculator() {
+  const [error, setError] = useState(false);
+  const [custom, setCustom] = useState(false);
+  const [tip, setTip] = useState(0);
+  const [bill, setBill] = useState("");
+  const [people, setPeople] = useState("");
+
+  function customHandler() {
+    setCustom(true);
+    setTip(0);
+  }
+
+  function peopleHandler(event: React.ChangeEvent) {
+    const value = (event.target as HTMLInputElement).value;
+    if (value === "0" || value === "") setError(true);
+    else setError(false);
+    setPeople(value);
+  }
+
+  function tipHandler(event: React.ChangeEvent) {
+    const value = (event.target as HTMLInputElement).value;
+    setCustom(false);
+    setTip(parseInt(value));
+  }
+
+  function billHandler(event: React.ChangeEvent) {
+    const value = (event.target as HTMLInputElement).value;
+    setBill(value);
+  }
+
+  function customAmountHandler(event: React.ChangeEvent) {
+    const value = (event.target as HTMLInputElement).value;
+    if (Number.isFinite(parseInt(value))) setTip(parseFloat(value));
+    if (value === "") setTip(0);
+  }
+
+  function resetHandler() {
+    setError(false);
+    setCustom(false);
+    setTip(0);
+    setBill("");
+    setPeople("");
+  }
+
+  const tipAmount =
+    error || people === "" || bill === ""
+      ? 0
+      : (parseInt(bill) * (tip / 100)) / parseInt(people);
+
+  const total =
+    error || people === "" || bill === ""
+      ? 0
+      : (parseInt(bill) * (1 + tip / 100)) / parseInt(people);
+
   return (
     <div className="tip-calculator-app">
       <header>
@@ -15,52 +70,137 @@ function TipCalculator() {
             <label htmlFor="bill">Bill</label>
             <div className="icon-wrapper">
               <img src={dollarIcon} className="icon" />
-              <input type="number" id="bill" name="bill" />
+              <input
+                type="number"
+                id="bill"
+                name="bill"
+                min="0"
+                value={bill}
+                onChange={billHandler}
+              />
             </div>
           </div>
           <fieldset>
             <legend>Select Tip %</legend>
-            <label htmlFor="5">5%</label>
-            <input type="radio" id="5" name="tip" value="5" />
-            <label htmlFor="10">10%</label>
-            <input type="radio" id="10" name="tip" value="10" />
-            <label htmlFor="15">15%</label>
-            <input type="radio" id="15" name="tip" value="15" />
-            <label htmlFor="25">25%</label>
-            <input type="radio" id="25" name="tip" value="25" />
-            <label htmlFor="50">50%</label>
-            <input type="radio" id="50" name="tip" value="50" />
-            <label htmlFor="custom" className="custom">
-              Custom
-            </label>
-            <input type="radio" id="custom" name="tip" value="custom" />
+            <div className="grid-container">
+              <label htmlFor="5" className={tip === 5 ? "selected" : undefined}>
+                5%
+              </label>
+              <input
+                type="radio"
+                id="5"
+                name="tip"
+                value="5"
+                onChange={tipHandler}
+              />
+              <label
+                htmlFor="10"
+                className={tip === 10 ? "selected" : undefined}
+              >
+                10%
+              </label>
+              <input
+                type="radio"
+                id="10"
+                name="tip"
+                value="10"
+                onChange={tipHandler}
+              />
+              <label
+                htmlFor="15"
+                className={tip === 15 ? "selected" : undefined}
+              >
+                15%
+              </label>
+              <input
+                type="radio"
+                id="15"
+                name="tip"
+                value="15"
+                onChange={tipHandler}
+              />
+              <label
+                htmlFor="25"
+                className={tip === 25 ? "selected" : undefined}
+              >
+                25%
+              </label>
+              <input
+                type="radio"
+                id="25"
+                name="tip"
+                value="25"
+                onChange={tipHandler}
+              />
+              <label
+                htmlFor="50"
+                className={tip === 50 ? "selected" : undefined}
+              >
+                50%
+              </label>
+              <input
+                type="radio"
+                id="50"
+                name="tip"
+                value="50"
+                onChange={tipHandler}
+              />
+              {!custom && (
+                <label htmlFor="custom" className="custom">
+                  Custom
+                </label>
+              )}
+              <input
+                type="radio"
+                id="custom"
+                name="tip"
+                value="custom"
+                onChange={customHandler}
+                checked={custom}
+              />
+              {custom && (
+                <input
+                  type="number"
+                  className="custom-amount"
+                  min="0"
+                  autoFocus
+                  onChange={customAmountHandler}
+                />
+              )}
+            </div>
           </fieldset>
-          <div className="field error">
+          <div className={error ? "field error" : "field"}>
             <div className="flex-container">
               <label htmlFor="people">Number of People</label>
               <p className="error-message">Can't be zero</p>
             </div>
             <div className="icon-wrapper">
               <img src={personIcon} className="icon" />
-              <input type="number" id="people" name="people" />
+              <input
+                type="number"
+                id="people"
+                name="people"
+                min="1"
+                value={people}
+                onChange={peopleHandler}
+              />
             </div>
           </div>
-          {/* <button>HO</button> */}
         </form>
         <div className="output">
           <div className="calculation">
             <p className="category">
               Tip Amount<span>/ person</span>
             </p>
-            <p className="result">$4.27</p>
+            <p className="result">${tipAmount.toFixed(2)}</p>
           </div>
           <div className="calculation">
             <p className="category">
               Total<span>/ person</span>
             </p>
-            <p className="result">$32.79</p>
+            <p className="result">${total.toFixed(2)}</p>
           </div>
-          <button>RESET</button>
+          <button onClick={resetHandler}>RESET</button>
         </div>
       </main>
     </div>
