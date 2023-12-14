b7e4aa3e8477c080f370150886f0fd74e1fdacc7
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-14 19:28:12 +0000

    Store customer testimonials in data file

---
+++ src/projects/junior/sunnyside-agency-landing-page-main/src/data/testimonials.ts
@@ -0,0 +1,32 @@
+import emilyImg from "../assets/images/image-emily.jpg";
+import thomasImg from "../assets/images/image-thomas.jpg";
+import jennieImg from "../assets/images/image-jennie.jpg";
+
+const testimonials = [
+  {
+    id: 1,
+    name: "Emily R.",
+    title: "Marketing Director",
+    comment:
+      "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadline were always hit.",
+    avatar: emilyImg,
+  },
+  {
+    id: 2,
+    name: "Thomas S.",
+    title: "Chief Operating Officer",
+    comment:
+      "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.",
+    avatar: thomasImg,
+  },
+  {
+    id: 3,
+    name: "Jennie R.",
+    title: "Business Owner",
+    comment:
+      "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    avatar: jennieImg,
+  },
+];
+
+export default testimonials;
