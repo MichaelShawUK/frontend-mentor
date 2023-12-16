e8c1256bc2044dabd620caa8fdc4696eed2a4ca5
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-16 22:33:48 +0000

    Display modal when mobile menu is active

--- src/projects/junior/blogr-landing-page-main/src/components/MobileMenu.tsx
+++ src/projects/junior/blogr-landing-page-main/src/components/MobileMenu.tsx
@@ -1,9 +1,29 @@
 import hamburger from "../assets/images/icon-hamburger.svg";
+import closeIcon from "../assets/images/icon-close.svg";
+import Nav from "./Nav";
+import AccountActions from "./AccountActions";
+
+import { useState } from "react";
 
 function MobileMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  const HamburgerIcon = <img src={hamburger} className="icon" onClick={open} />;
+  const CloseIcon = <img src={closeIcon} className="icon" onClick={close} />;
+  const Icon = isOpen ? CloseIcon : HamburgerIcon;
+
   return (
     <div className="mobile-menu">
-      <img src={hamburger} />
+      {Icon}
+      {isOpen && (
+        <div className="modal">
+          <Nav />
+          <AccountActions />
+        </div>
+      )}
     </div>
   );
 }
