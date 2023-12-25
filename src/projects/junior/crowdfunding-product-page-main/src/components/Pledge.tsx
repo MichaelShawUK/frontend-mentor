b59d61752cf3ca74ecfa95d6a002396ed9e350d2
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-25 21:32:36 +0000

    Add pledge form

--- src/projects/junior/crowdfunding-product-page-main/src/components/Pledge.tsx
+++ src/projects/junior/crowdfunding-product-page-main/src/components/Pledge.tsx
@@ -15,6 +15,14 @@ function Pledge({ pledge }: { pledge: RewardType }) {
           <span>{pledge.remaining}</span> left
         </p>
       )}
+      <form>
+        <label htmlFor={`pledge${pledge.id}`}>Enter your pledge</label>
+        <div className="input-wrapper">
+          <input type="number" name="pledge" id={`pledge${pledge.id}`} />
+        </div>
+        <button className="primary">Continue</button>
+        <p className="error-message">Please enter ${pledge.minimum} or more</p>
+      </form>
     </div>
   );
 }
