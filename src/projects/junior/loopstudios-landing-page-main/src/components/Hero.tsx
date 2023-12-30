3b46181cab94847eccb13ae50b6e3b4678509e1a
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-30 19:01:01 +0000

    Position mobile hero elements

--- src/projects/junior/loopstudios-landing-page-main/src/components/Hero.tsx
+++ src/projects/junior/loopstudios-landing-page-main/src/components/Hero.tsx
@@ -1,6 +1,14 @@
-import hero from "../assets/images/desktop/image-hero.jpg";
+import heroDesktop from "../assets/images/desktop/image-hero.jpg";
+import heroMobile from "../assets/images/mobile/image-hero.jpg";
+import DeviceModeContext from "../context/DeviceModeContext";
+
+import { useContext } from "react";
 
 function Hero() {
+  const device = useContext(DeviceModeContext);
+
+  const hero = device === "mobile" ? heroMobile : heroDesktop;
+
   return (
     <div className="hero">
       <img src={hero} />
