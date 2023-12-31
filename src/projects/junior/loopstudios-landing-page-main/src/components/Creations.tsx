215ffbb29a877ad77286e7f4f8c8a2c37165bdee
MichaelShawUK <MichaelShawUK@gmail.com>
2023-12-31 13:20:20 +0000

    Move h2 styling into index file

---
+++ src/projects/junior/loopstudios-landing-page-main/src/components/Creations.tsx
@@ -0,0 +1,10 @@
+function Creations() {
+  return (
+    <div className="creations">
+      <h2>OUR CREATIONS</h2>
+      <button>SEE ALL</button>
+    </div>
+  );
+}
+
+export default Creations;
--- src/projects/junior/loopstudios-landing-page-main/src/components/Main.tsx
+++ src/projects/junior/loopstudios-landing-page-main/src/components/Main.tsx
@@ -1,9 +1,11 @@
 import Summary from "./Summary";
+import Creations from "./Creations";
 
 function Main() {
   return (
     <main>
       <Summary />
+      <Creations />
     </main>
   );
 }
