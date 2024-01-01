bacdd04c6947471f96c8b930fcc1e4f72a75ac12
MichaelShawUK <MichaelShawUK@gmail.com>
2024-01-01 17:20:30 +0000

    Position testimonials grid items

--- src/projects/junior/testimonials-grid-section-main/src/components/Testimonial.tsx
+++ src/projects/junior/testimonials-grid-section-main/src/components/Testimonial.tsx
@@ -3,11 +3,11 @@ import { TestimonialType } from "../data/testimonials";
 function Testimonial({ testimonial }: { testimonial: TestimonialType }) {
   return (
     <div className={`testimonial ${testimonial.card}`}>
-      <img src={testimonial.avatar} />
+      <img className="avatar" src={testimonial.avatar} />
       <p className="name">{testimonial.name}</p>
       <p className="status">{testimonial.status}</p>
       <h3 className="headline">{testimonial.headline}</h3>
-      <p className="review">{testimonial.review}</p>
+      <p className="review">"{testimonial.review}"</p>
     </div>
   );
 }
