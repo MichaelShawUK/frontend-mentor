db7fcbfa4d80727c51b54d41a62f950155f5de4a
MichaelShawUK <MichaelShawUK@gmail.com>
2024-01-07 19:49:33 +0000

    Add links to nav component

--- src/projects/junior/insure-landing-page-master/src/App.tsx
+++ src/projects/junior/insure-landing-page-master/src/App.tsx
@@ -1,8 +1,11 @@
 import "./styles/css/index.css";
 
+import Nav from "./components/Nav";
+
 function Insure() {
   return (
     <div className="insure">
+      <Nav />
       <h1>heading</h1>
       <p>INSURE</p>
     </div>
---
+++ src/projects/junior/insure-landing-page-master/src/components/Nav.tsx
@@ -0,0 +1,22 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#work">HOW WE WORK</a>
+        </li>
+        <li>
+          <a href="#blog">BLOG</a>
+        </li>
+        <li>
+          <a href="#account">ACCOUNT</a>
+        </li>
+        <li>
+          <a href="#plans">VIEW PLANS</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
