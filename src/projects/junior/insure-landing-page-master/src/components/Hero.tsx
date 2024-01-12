1a360ac4d0b474a5be5dd059f541120a904988e4
MichaelShawUK <MichaelShawUK@gmail.com>
2024-01-12 15:33:31 +0000

    Layout hero component for desktop devices

--- src/projects/junior/insure-landing-page-master/src/components/Hero.tsx
+++ src/projects/junior/insure-landing-page-master/src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import mobileHero from "../assets/images/image-intro-mobile.jpg";
 import desktopHero from "../assets/images/image-intro-desktop.jpg";
 
 import DeviceModeContext from "../context/DeviceModeContext";
@@ -8,20 +7,21 @@ import { useContext } from "react";
 function Hero() {
   const device = useContext(DeviceModeContext);
 
-  const image = device === "mobile" ? mobileHero : desktopHero;
-
   return (
     <section className="hero">
-      <img src={image} />
-      <div className="introduction">
-        <h1>Humanizing your insurance.</h1>
-        <p>
-          Get your life insurance coverage easier and faster. We blend our
-          expertise and technology to help you find the plan that's right for
-          you. Ensure you and your loved ones are protected.
-        </p>
-        <button>VIEW PLANS</button>
+      <div className="wrapper">
+        <img src={desktopHero} />
+        <div className="introduction">
+          <h1>Humanizing your insurance.</h1>
+          <p>
+            Get your life insurance coverage easier and faster. We blend our
+            expertise and technology to help you find the plan that's right for
+            you. Ensure you and your loved ones are protected.
+          </p>
+          <button>VIEW PLANS</button>
+        </div>
       </div>
+      {device === "desktop" && <div className="overlay"></div>}
     </section>
   );
 }
