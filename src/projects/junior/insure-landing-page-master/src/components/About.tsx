56e12576af855554cb0cbe3f1a4fb78f52fbb1b6
MichaelShawUK <MichaelShawUK@gmail.com>
2024-01-13 12:29:16 +0000

    Style about component for mobile devices

---
+++ src/projects/junior/insure-landing-page-master/src/components/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <article className="about">
+      <h1>Find out more about how we work</h1>
+      <button>HOW WE WORK</button>
+    </article>
+  );
+}
+
+export default About;
--- src/projects/junior/insure-landing-page-master/src/components/Main.tsx
+++ src/projects/junior/insure-landing-page-master/src/components/Main.tsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero";
 import Features from "./Features";
+import About from "./About";
 
 function Main() {
   return (
     <main>
       <Hero />
       <Features />
+      <About />
     </main>
   );
 }
