90f3648ac7a7908f624a5dfaa7f2086a19102e3a
MichaelShawUK <MichaelShawUK@gmail.com>
2024-01-20 16:32:49 +0000

    Add basic styles

--- src/projects/junior/huddle-landing-page-with-alternating-feature-blocks-master/src/App.tsx
+++ src/projects/junior/huddle-landing-page-with-alternating-feature-blocks-master/src/App.tsx
@@ -1,7 +1,11 @@
+import "./styles/css/index.css";
+
 function HuddleAlternating() {
   return (
     <div className="huddle-alternating">
       <p>huddle</p>
+
+      <img src="src/projects/junior/huddle-landing-page-with-alternating-feature-blocks-master/design/desktop-design.jpg" />
     </div>
   );
 }
